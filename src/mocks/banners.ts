05d53c62fc2ea05a31d37b884f8c07376d1f2a27
Daniel <taehyun_fe@naver.com>
2026-01-05 23:28:42 +0900

    fix: 영문 배너 description에 줄바꿈 추가

--- src/mocks/banners.ts
+++ src/mocks/banners.ts
@@ -29,7 +29,7 @@ export const banners: Banner[] = [
     },
     description: {
       ko: '디센트 지문인증형 지갑으로\n한 층 더 강화된 보안을 경험하세요!',
-      en: 'Enhance your security with D\'CENT biometric wallet',
+      en: 'Enhance your security\nwith D\'CENT biometric wallet',
     },
     ctaText: {
       ko: '구매하기',
@@ -48,7 +48,7 @@ export const banners: Banner[] = [
     },
     description: {
       ko: '새로운 디센트 블로그를 방문하여\n최신 업데이트를 먼저 확인해보세요!',
-      en: 'Visit the new D\'CENT Blog to explore the latest updates first!',
+      en: 'Visit the new D\'CENT Blog\nto explore the latest updates first!',
     },
     ctaText: {
       ko: '확인하기',
